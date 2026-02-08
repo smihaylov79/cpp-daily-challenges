b0ad53896df3aecfe6c2196efb8daeae1aa7f4af
Ivo <ivokoychev06@icloud.com>
2026-02-08 14:07:11 +0200

    Day 1 hi stoyan

---
+++ solutions/easy/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter first number: " << endl;
+    cin >> a;
+    cout << "Enter second number:" << endl;
+    cin >> b;
+
+    int sum = a + b;
+    cout << "the sum is: " << sum << endl;
+    return 0;
+}
